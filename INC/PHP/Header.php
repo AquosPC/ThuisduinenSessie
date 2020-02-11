7816817015253a47b3d9ca12bcce21c49069d71a
Aquos <knowak315@gmail.com>
2020-02-11 11:29:12 +0100

    added header with navbar

--- INC/PHP/Header.php
+++ INC/PHP/Header.php
@@ -4,13 +4,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>THuisduinen</title>
-    <link rel="stylesheet" href="Main.css">
+    <link rel="stylesheet" href="https:localhost/ThuisduinenSessie/INC/CSS/Header.css">
 </head>
 <body>
-    <nav>
-    <a href="www.google.com">test 1</a>
-    <a href="www.google.com">test 2</a>
-    <a href="www.google.com">test 3</a>
+    <img class="Logo" src="https:localhost/ThuisduinenSessie/Assets/thuisduinen2050-1000px.png" alt="Logo">
+    <nav class="flex-nav nav">
+        <div class="navdiv">
+            <a class="navlinks" href="www.google.com">Welkom</a>
+            <a class="navlinks" href="www.google.com">Over ons</a>
+            <a class="navlinks" href="www.google.com">Bewoners</a>
+            <a class="navlinks" href="www.google.com">Projecten</a>
+            <a class="navlinks" href="www.google.com">Contact</a>
+        </div>
     </nav>
 </body>
 </html>
