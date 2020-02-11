06da26d3a5a69392cede36f64e6dfddd562aba74
Aquos <knowak315@gmail.com>
2020-02-11 14:54:36 +0100

    frontpage done like its nothing

--- INC/PHP/Header.php
+++ INC/PHP/Header.php
@@ -4,10 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>THuisduinen</title>
-    <link rel="stylesheet" href="https:localhost/ThuisduinenSessie/INC/CSS/Header.css">
+    <link rel="stylesheet" href="https://localhost/ThuisduinenSessie/INC/CSS/Header.css">
+    <link href="https://fonts.googleapis.com/css?family=Ubuntu:500&display=swap" rel="stylesheet"> 
+    <script src="https://localhost/ThuisduinenSessie/INC/JS/Sticky.js"></script>
 </head>
 <body>
-    <img class="Logo" src="https:localhost/ThuisduinenSessie/Assets/thuisduinen2050-1000px.png" alt="Logo">
+    <div id="navbar">
+    <img class="Logo" src="https://localhost/ThuisduinenSessie/Assets/thuisduinen2050-1000px.png" alt="Logo">
     <nav class="flex-nav nav">
         <div class="navdiv">
             <a class="navlinks" href="www.google.com">Welkom</a>
@@ -17,5 +20,9 @@
             <a class="navlinks" href="www.google.com">Contact</a>
         </div>
     </nav>
+    </div>
+    <div class="headimg parallax">
+
+    </div>
 </body>
 </html>
--- Index.php
+++ Index.php
@@ -1,5 +1,58 @@
 <?php 
     Require "INC/PHP/Header.php";
 ?>
-
+<head>
+    <link rel="stylesheet" href="https:localhost/ThuisduinenSessie/INC/CSS/Head.css">
+</head>
+<body class="pageBG">
+    <div>
+    <div class="maincard">
+        <h2 class="bold">Thuisduinen2050</h2>
+        <h3 class="bold">Alles voor en door bewoners</h3>
+        <p class="semibold">
+            Huisduinen is een van de populairste woonlocaties in de gemeente Den Helder en de rust en ruimte van de natuurgebieden worden door bewoners vanuit heel Den Helder gebruikt voor natuurbeleving en recreatie. 
+            In de loop der jaren is de vraag ontstaan hoe  de bewoners van Huisduinen kunnen bijdragen aan een toekomstbestendige ontwikkeling van hun woon- en leefomgeving, waarbij de identiteit en het gebiedseigen karakter behouden blijft. 
+            Vanuit dit initiatief is 'Thuisduinen2050' ontstaan. 
+        </p>
+        <p class="semibold">
+            Huisduinen als 'kenniswerkplaats' 
+        </p>
+        <p>
+            Inholland heeft samen met de Stichting Huisduiner Belang een kenniswerkplaats opgericht waarin studenten aan de slag gaan met vraagstukken uit Huisduinen die te maken hebben met Samenwerking en Communicatie, Voeding en Gezondheid, Energie en Duurzaamheid, Transport en Infrastructuur en Zorg en Welzijn.
+                <br><br>
+            In de eerste helft van schooljaar 2019-2020 gaan zesentwintig studenten van Hogeschool Inholland en negen studenten ROC Kop van Noord-Holland aan de slag met vier projecten in de kenniswerkpaats, een nieuwe vorm van onderwijs. 
+            In deze methodiek werken studenten samen met docenten, bedrijven én bewoners aan oplossingen voor actuele interprofessionele vraagstukken in de samenleving. 
+            Het mes snijdt aan verschillende kanten tegelijk!
+                <br><br>
+            Heb jij als bewoner ook ideeën voor een project of wil jij partciperen in bestaande projecten? Neem dan contact met ons op via ons contactformulier.
+        </p>
+    </div>
+    <div class="parallaxwithtext">
+        <div class="divwithtext">
+        <h1>Bezienswaardigheden</h1>
+        <p>Naast het strand, duinen en natuur is Huisduinen vooral bekend van Fort Kijkduin. 
+            Het fort, daterend van 1811, is onderdeel van de Stelling Den Helder, een verdedigingslinie die werd gebouwd onder Napoleon. 
+            Het fort is tegenwoordig een museum, dat de geschiedenis vertelt van de periode dat het een fort was. 
+            In het fort bevindt zich ook een Noordzee-aquarium.
+        </p>
+        <p>Klik hier voor een filmpje</p>
+        </div>
+    </div>
+    <div class="maincard">
+        <div class="Blogdiv">
+            <div class="Blogimage">
+                <img  class="center" src="https://localhost/ThuisduinenSessie/Assets/education-12.960x0.jpg" alt="Education">
+            </div>
+            <div>
+                <h2>Test title</h2>
+                <p>11/02/2020 (test)</p>
+            </div>
+            <div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic itaque adipisci tempore, eum necessitatibus accusantium provident illum perferendis odit, dolores commodi odio neque a? Incidunt libero excepturi exercitationem dolore assumenda.</p>
+                <a href="https://www.google.com"><button class="button">Meer Lezen</button></a>
+            </div>
+        </div>
+    </div>
+    </div>
+</body>
 <?php require("INC/PHP/footer.php") ?>
