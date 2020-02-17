5d95991ef98365f562dd92899b884e99cdb2febd
Aquos <knowak315@gmail.com>
2020-02-17 10:36:23 +0100

    Update Index.php

--- Index.php
+++ Index.php
@@ -44,11 +44,15 @@
                 <img  class="center" src="https://localhost/ThuisduinenSessie/Assets/education-12.960x0.jpg" alt="Education">
             </div>
             <div>
-                <h2>Test title</h2>
+                <h2>PAK JIJ EEN GOUDEN TICKET T.W.V. € 5.000?</h2>
                 <p>11/02/2020 (test)</p>
             </div>
             <div>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic itaque adipisci tempore, eum necessitatibus accusantium provident illum perferendis odit, dolores commodi odio neque a? Incidunt libero excepturi exercitationem dolore assumenda.</p>
+                <p>Ben jij tussen de 16 en 27 jaar oud en vraag jij jezelf wel eens af wat voor werk nu echt bij je past? 
+                Waar jij energie van krijgt en waar je goed in bent?
+                In het Gouden Kans project doe je in zes maanden tijd de meest bijzondere werkervaring op met een ‘goed gevoel project’. 
+                Jij gaat aan een project werken bij een organisatie die er toe doet. 
+                Je volgt trainingen en ontwikkelt nieuwe skills. Je wordt gecoacht in de praktijk, zodat jij je talenten ontdekt.</p>
                 <a href="https://www.google.com"><button class="button">Meer Lezen</button></a>
             </div>
         </div>
