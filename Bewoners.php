eb88f54dc27b5420be2e9f4727c9913df0eafe6c
Aquos <knowak315@gmail.com>
2020-02-17 13:52:30 +0100

    Added & Finished Bewoners.php

---
+++ Bewoners.php
@@ -0,0 +1,12 @@
+<?php 
+    Require "INC/PHP/Header.php";
+?>
+<head>
+    <link rel="stylesheet" href="https:localhost/ThuisduinenSessie/INC/CSS/Head.css">
+</head>
+<body class="pageBG">
+    <div class="maincard">
+        <h2 class="textcenter">Bewoners</h2>
+    </div>
+</body>
+<?php require("INC/PHP/footer.php") ?>
