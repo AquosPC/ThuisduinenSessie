3d45d66fbe527e072b79b54e1001ffca0240efd7
LordVis <robert1842000@hotmail.nl>
2020-03-17 18:48:49 +0100

    ffd

--- Contact.php
+++ Contact.php
@@ -20,10 +20,10 @@
         </h3>
         <br>
         <br>
-        <form action="INC/PHP/Mail.php" method="post">
-            <input type="text" name="Name" id="Name" placeholder="Naam"><br><br>
-            <input type="email" name="Mail" id="Mail"placeholder="E-mail"><br><br>
-            <textarea name="Text" id="Text" cols="50" rows="10" placeholder="Bericht" ></textarea><br><br>
+        <form action="INC/PHP/_contact.php" method="post">
+            <input type="text" name="name" id="Name" placeholder="Naam"><br><br>
+            <input type="email" name="mail" id="Mail"placeholder="E-mail"><br><br>
+            <textarea name="text" id="text" cols="50" rows="10" placeholder="Bericht" ></textarea><br><br>
             <input class="button" type="submit" value="Verstuur">
         </form>
     </div>
--- INC/PHP/_contact.php
+++ INC/PHP/_contact.php
@@ -1,11 +1,11 @@
 <?php
 session_start();
-if (ISSET($_POST['vnaam'])) {
+if (ISSET($_POST['name'])) {
 
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "ThuisduinenSessie";
+$dbname = "thuisduinensessie";
 $link = mysqli_connect("$servername","$username","","$dbname");
 if (!$link) {	
 	die('Could not connect: ' . mysqli_connect_error()); }
@@ -14,33 +14,23 @@ else {
 }
 
 // Persoonlijke informatie.
-$var1 = mysqli_real_escape_string($link, $_POST['vnaam']);
-$var2 = mysqli_real_escape_string($link, $_POST['tussenvoegsel']);
-$var3 = mysqli_real_escape_string($link, $_POST['anaam']);
-$var4 = mysqli_real_escape_string($link, $_POST['straatnaam']);
-$var5 = mysqli_real_escape_string($link, $_POST['nummer']);
-$var6 = mysqli_real_escape_string($link, $_POST['postcode']);
-$var7 = mysqli_real_escape_string($link, $_POST['plaatsnaam']);
-$var8 = mysqli_real_escape_string($link, $_POST['toevoeging']);
-$var9 = mysqli_real_escape_string($link, $_POST['aanhef']);
-$var10 = mysqli_real_escape_string($link, $_POST['onderwerp']);
-$var11 = mysqli_real_escape_string($link, $_POST['message']);
-$var12 = mysqli_real_escape_string($link, $_POST['email']);
-$var13 = mysqli_real_escape_string($link, $_POST['telefoon_nummer']);
+$var1 = mysqli_real_escape_string($link, $_POST['name']);
+$var2 = mysqli_real_escape_string($link, $_POST['mail']);
+$var3 = mysqli_real_escape_string($link, $_POST['text']);
 
 
 
 // Inserts data into the database.
-$sql = "INSERT INTO `contactform` (firstName, insertion, lastName, streetName, houseNummer, zipcode, city, addOn, salutation, topic, message, email, tellNumber)
-VALUES ('$var1', '$var2', '$var3', '$var4', '$var5', '$var6', '$var7', '$var8', '$var9', '$var10', '$var11', '$var12', '$var13')";
+$sql = "INSERT INTO `contactform` (userName, userEmail, userMessage)
+VALUES ('$var1', '$var2', '$var3')";
 
 $result = mysqli_query($link, $sql);
 
 // Echo's back so the user knows that the data has been uploaded.
-$message = "Your message has been sent!";
+$message = "U bericht is verzonden!";
 echo "<script type='text/javascript'>alert('$message');</script>";
 
-header( "Refresh:0.01; ../Contact.php");
+header( "Refresh:0.01; ../../Contact.php");
 
 // Closes the connection.
 mysqli_close($link);
